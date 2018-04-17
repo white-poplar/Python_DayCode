583dbdb45d4e4012201cb3511c6ef79f8e97cd0d
白楊 <BYH5566@163.com>
2018-04-17 15:04:37 +0800

    0004

---
+++ 0004/word.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-03-21 10:16:20
+# @Author  : poplar. (BYH5566@gmail.com)
+# @Link    : https://white-poplar.github.io
+# @Version : $Id$
+
+import os
+
+
+
+if __name__ == '__main__':
+    pass
